8ff38b7e49c2e91faf67b81115cf54233caf8931
Divyarajalakshmi Natarajan <divya@Abilashs-MacBook-Pro.local>
2017-03-15 09:54:32 -0700

    working with mongo db

--- app/routes.js
+++ app/routes.js
@@ -1,18 +1,18 @@
 var mongojs = require('mongojs');
-// var mongoose = require('mongoose').Mongoose;
+var mongoose = require('mongoose');
 var UserDB = mongojs('User', ['Userdb']);
-// mongoose.connect('mongodb://localhost/UserDB');
+mongoose.connect('mongodb://localhost/UserDB');
 
 module.exports=function(app){
 	app.post('/user',function(req,res){
-console.log("post request inserted");
+console.log("post request receive");
 
         var data = req.body;
         console.log(data);
-        res.json(data);
-//         UserDB.Userdb.insert(req.body,function(err,docs){
-// console.log("in server"+docs);
-// res.json(docs);
-        // });
+        // res.json(data);
+       UserDB.Userdb.insert(req.body,function(err,docs){
+ console.log("in server"+docs);
+  res.json(data);
+      });
 	});
 }
